bb639b939d0d3a70f2bd95b9d5ad34fb32b880cc
Hyuck <lhg1304@nexmore.co.kr>
2023-02-03 16:20:46 +0900

    o MainActivity Preview 제거

--- app/src/main/java/io/github/hyuck9/progressitem/sample/MainActivity.kt
+++ app/src/main/java/io/github/hyuck9/progressitem/sample/MainActivity.kt
@@ -3,22 +3,8 @@ package io.github.hyuck9.progressitem.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import io.github.hyuck9.progressitem.ProgressItem
 import io.github.hyuck9.progressitem.sample.ui.theme.ProgressItemTheme
 import io.github.hyuck9.progressitem.sample.ui.view.SampleScreen
 
@@ -31,42 +17,5 @@ class MainActivity : ComponentActivity() {
                 SampleScreen(modifier = Modifier.fillMaxSize())
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ProgressItemTheme {
-        Greeting("Android")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ProgressItemPreview() {
-    ProgressItemTheme {
-        ProgressItem(
-            modifier = Modifier
-                .fillMaxWidth()
-                .shadow(elevation = 3.dp, shape = RoundedCornerShape(10.dp))
-                .clip(RoundedCornerShape(10.dp))
-                .background(Color(0xfff9f2ff)),
-            brush = Brush.horizontalGradient(listOf(Color(0xff86f7fa), Color(0xff9b86fa))),
-            percent = 60f,
-            content = @Composable {
-                Text(
-                    text = "테스트 입니다",
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White
-                )
-            }
-        )
     }
 }
